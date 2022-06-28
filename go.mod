045f0209f2bcd8a75e50424b62a4cfcfc3aa4dc6
hernandezbe <estebaneduardoh@gmail.com>
2022-06-28 12:54:28 -0400

    v1

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/hernandezbe/figuras
+
+go 1.18
---
+++ medidas.go
@@ -0,0 +1,14 @@
+package figuras
+
+import "fmt"
+
+type Geometrica interface {
+	area() float64
+	perimetro() float64
+}
+
+func Medidas(g Geometrica) {
+	fmt.Println("Medidas:", g)
+	fmt.Println("Area:", g.area())
+	fmt.Println("Perimetro", g.perimetro())
+}
